63be87ae05c8c228e1c4a31f14ff10e643ed0ec0
hexbioc <vaibhav.nayak@gmail.com>
2020-07-29 14:37:50 +0530

    Update to make posting optional, and use fuzzy time

--- server/app.py
+++ server/app.py
@@ -3,13 +3,15 @@ import logging
 from datetime import datetime
 from pprint import pprint
 
+import dateparser
 import requests
 from flask import Flask, current_app, request
 from googleapiclient.errors import HttpError
 from slack.errors import SlackApiError
 
-from .after_this_response import AfterThisResponse
-from .services import calendar_service, slack_service
+import server.config as config
+from server.after_this_response import AfterThisResponse
+from server.services import calendar_service, slack_service
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
@@ -50,13 +52,12 @@ def add():
     date_str = request.form["text"]
     log.info("received text for /ooo slash command - %s", date_str)
     try:
-        start_date = datetime.strptime(date_str, "%d-%m-%Y")
-    except ValueError:
-        log.error(
-            "received incorrect date format where dd-mm-yyyy was expected - %s",
-            date_str,
-        )
-        return "Date has to be provided in dd-mm-yyyy format."
+        start_date = dateparser.parse(text, settings={"PREFER_DATES_FROM": "futures"})
+        if start_date is None:
+            raise ValueError("no date found")
+    except ValueError as e:
+        log.error("error parsing date in string '%s' - %s", text, e)
+        return "I couldn't figure that out. Maybe share the date in dd-mm-yyyy format?"
 
     # Capture required parameters from request
     response_url = request.form["response_url"]
@@ -86,16 +87,21 @@ def add():
             start_date.strftime("%d-%m-%Y"),
         )
         # Send a private ephemeral response
-        response_text = f"Created event - <{event_link}|{event_name}>. Don't forget to apply on <https://brokentusk.greythr.com/|greytHR> too!  :palm_tree:"
-        requests.post(response_url, json={"text": response_text})
-        # Send a message to #ooo about this event
         long_date = start_date.strftime("%A, %d %B %Y")
-        try:
-            slack_service.chat_postMessage(
-                channel="#ooo",
-                text=f"<@{user_id}> will be OOO on {long_date}  :palm_tree:",
-            )
-        except SlackApiError as e:
-            log.error("error occurred while sending message - %s", e)
+        response_text = (
+            f"Created event for {long_date} - <{event_link}|{event_name}>."
+            + " Don't forget to apply on <https://brokentusk.greythr.com/|greytHR> too!  :palm_tree:"
+        )
+        requests.post(response_url, json={"text": response_text})
+
+        if config.POST_TO_OOO:
+            # Send a message to #ooo about this event
+            try:
+                slack_service.chat_postMessage(
+                    channel="#ooo",
+                    text=f"<@{user_id}> will be OOO on {long_date}  :palm_tree:",
+                )
+            except SlackApiError as e:
+                log.error("error occurred while sending message - %s", e)
 
     return "", 200
--- server/config.py
+++ server/config.py
@@ -2,6 +2,8 @@ import base64
 import os
 import tempfile
 
+_truthy_values_ = ["1", "true", "yes", "y"]
+
 OOO_CALENDAR_ID = os.getenv("OOO_CALENDAR_ID", "")
 SLACK_TOKEN = os.getenv("SLACK_TOKEN", "")
 _b64_sa_key_file_ = os.getenv("B64_SA_KEY_FILE", "")
@@ -11,3 +13,4 @@ _sa_key_file_.write(base64.b64decode(_b64_sa_key_file_))
 _sa_key_file_.flush()
 
 SA_KEY_PATH = _sa_key_file_.name
+POST_TO_OOO = os.getenv("POST_TO_OOO", "False").strip().lower() in _truthy_values_
